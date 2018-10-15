5ef4e69b0d51644ba7eb6d7c4de4e903383b729d
Olga R <rolya85@bk.ru>
2018-10-15 13:39:34 +0300

    Threads

---
+++ Advanced-Lesson-6-Multithreading/Advanced-Lesson-6-Multithreading/Lesson.Processes.cs
@@ -0,0 +1,113 @@
+﻿using Advanced_Lesson_6_Diagnostic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advanced_Lesson_6_Multithreading
+{
+    public static partial class Lesson
+    {
+        public static void SinglePlayerExample()
+        {
+            var player = new Player("Player 1");
+            player.Play();            
+
+            Diagnostic.ListAllRunningProcesses();
+            Diagnostic.ListAllProcessThreads();
+            Diagnostic.ListAllProcessCodeModules();
+            Diagnostic.ListAllAppDomains();
+        }
+
+        public static void AppDomainPlayersExample()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var index = i + 1;
+                AppDomain anotherAD = AppDomain.CreateDomain($"Player {index}");
+                try
+                {
+                    var assempbly = anotherAD.Load("Advanced-Lesson-6-AppDomain-Player");
+                    assempbly.EntryPoint.Invoke(null, new object[] { new string[] { (index).ToString() } });
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Diagnostic.ListAllAppDomains();
+        }
+
+        public static void MultiThreadingExample()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    var player = new Player($"Thread {i}", i);
+                    player.Play();
+                });
+
+                thread.Start();
+
+                Console.WriteLine($"Diagnostic from thread #{i}");
+                Diagnostic.ListAllProcessThreads();
+            }
+
+            Console.WriteLine("Diagnostic from main thread");
+            Diagnostic.ListAllProcessThreads();
+        }
+
+        public static void ThreadPoolExample()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    var player = new Player($"Thread {i}", i);
+                    player.Play();
+                });
+
+                
+                Console.WriteLine($"Diagnostic from thread #{i}");
+                Diagnostic.ListAllProcessThreads();
+            }
+
+            Console.WriteLine("Diagnostic from main thread");
+            Diagnostic.ListAllProcessThreads();
+        }
+    }  
+
+
+    public class Player
+    {
+        public string Name { get; set; }
+        private int? _color;
+
+        public Player(string name, int? color = null)
+        {
+            this.Name = name;
+            this._color = color;
+        }        
+
+        public void Play()
+        {
+            var counter = 0;
+
+            while(counter ++ < 10)
+            {
+                if (this._color.HasValue)
+                    Console.BackgroundColor = (ConsoleColor)_color;
+
+                Console.WriteLine($"Playing from instance {this.Name}");
+
+                Thread.Sleep(500);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Advanced-Lesson-6-Multithreading/Advanced-Lesson-6-Multithreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Lesson_6_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lesson.SinglePlayerExample();
+            //Lesson.AppDomainPlayersExample();
+            //Lesson.MultiThreadingExample();
+            Lesson.ThreadPoolExample();
+
+            Console.ReadLine();
+        }
+    }
+}
